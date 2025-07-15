43ee6712f070ca7e3fc721cf60c12ed9373e8c5b
Taha Ghazi <68954755+tahaghazy@users.noreply.github.com>
2025-07-16 01:27:48 +0300

    Fix issue

--- src/apps/vehicles/api/serializers.py
+++ src/apps/vehicles/api/serializers.py
@@ -53,7 +53,7 @@ class VehicleListSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'brand', 'brand_name', 'model', 'year', 'price', 'currency',
             'body_type', 'color', 'mileage', 'engine_type', 'transmission',
-             'is_featured', 'primary_image', "is_negotiable", "created_at", "contract_type", "staff_only",
+             'is_featured', 'primary_image', "is_negotiable", "created_at", "staff_only",
             "is_available", "status", "available_units","type"
         ]
 
